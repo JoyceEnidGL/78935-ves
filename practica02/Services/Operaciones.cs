3ebf0a3cfb5bf19dda541e803fa401b840ae2f5c
Joyce <jegalo2001@gmail.com>
2023-03-02 15:11:30 -0600

    practica 02

---
+++ practica02/Services/Operaciones.cs
@@ -0,0 +1,20 @@
+using System;
+using WSDL.mensajes;
+
+namespace WSDL.operaciones
+{
+    public class Operaciones : Mensajes
+    {
+        private List<string> name = new List<string>();
+        public string Saludar(string nombre)
+        {
+            name.Add(nombre);
+            string msj = "Hola " + nombre;
+            return msj;
+        }
+        public string Mostrar(int id)
+        {
+            return name[id];
+        }
+    }
+}
